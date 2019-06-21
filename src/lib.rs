503da5114534ba8324adbdce37fb7aa4a56361c1
Jakub Syty <jakub.syty@euvic.pl>
2019-06-21 17:07:12 +0200

    Interop with external languages

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate byteorder;
+extern crate ole32;
 
 mod fingerprinting;
 mod input;
@@ -8,6 +9,52 @@ use input::*;
 use fingerprinting::codegen_generator;
 use std::path::Path;
 use crate::structures::fingerprint::Fingerprint;
+use std::slice::{from_raw_parts, from_raw_parts_mut};
+use std::str::from_utf8;
+use crate::structures::code_segment::CodeSegment;
+use std::mem::size_of;
+use ole32::CoTaskMemAlloc;
+
+/// this function is for use in external languages
+/// C# example:
+/// [DllImport("codegen.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+/// unsafe static extern uint calculate_for_raw_samples_c([In] byte[] buffer, [In] uint bufferLength, [Out] CodeSegment** array);
+///
+/// calling:
+/// var bytes = Encoding.UTF8.GetBytes($@"{basePath}raw16");
+/// CodeSegment* segments;
+/// var length = calculate_for_raw_samples_c(bytes, (uint)bytes.Length, &segments);
+#[no_mangle]
+pub unsafe extern "C" fn calculate_for_raw_samples_c(path: *const u8, path_length: u32, array_ptr: *mut *mut CodeSegment) -> u32
+{
+    let slice = from_raw_parts(path, path_length as usize);
+    let path_encoded = match from_utf8(slice) {
+        Ok(v) => v,
+        Err(_) => {
+            println!("Error during parsing path");
+            return 0
+        },
+    };
+
+    match calculate_for_raw_samples(path_encoded) {
+        Ok(fingerprint) => {
+            let length = fingerprint.codes.len();
+            let bytes_to_alloc = length * size_of::<CodeSegment>();
+            *array_ptr = CoTaskMemAlloc(bytes_to_alloc as u64) as *mut CodeSegment;
+
+            let array = from_raw_parts_mut(*array_ptr, length);
+            for (index, code_segment) in array.iter_mut().enumerate() {
+                code_segment.code = fingerprint.codes[index].code;
+                code_segment.time = fingerprint.codes[index].time;
+            }
+            return length as u32
+        },
+        Err(e) => {
+            println!("Error during fingerprinting: {}", e);
+            return 0
+        },
+    }
+}
 
 pub fn calculate_for_raw_samples<P>(path: P) -> Result<Fingerprint, String>
     where P : AsRef<Path>
@@ -17,7 +64,7 @@ pub fn calculate_for_raw_samples<P>(path: P) -> Result<Fingerprint, String>
     Ok(result)
 }
 
-pub fn calculate_for_audio_file<P>(path: P) -> Result<Fingerprint, String>
+pub fn calculate_for_audio_file<P>(_path: P) -> Result<Fingerprint, String>
     where P : AsRef<Path>
 {
     // TODO : call ffmpeg for raw samples output
--- src/structures/code_segment.rs
+++ src/structures/code_segment.rs
@@ -1,4 +1,5 @@
 #[derive(PartialEq, Debug)]
+#[repr(C)]
 pub struct CodeSegment {
     pub code: i32,
     pub time: i32,
